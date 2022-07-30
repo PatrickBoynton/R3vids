d1ae339a262d265bccd7acc11a35b470b5a1f686
PatrickBoynton <jpboynton3@gmail.com>
2022-07-30 18:20:15 -0400

    Added logic to add the paths to the database.

--- controllers/videoListController.js
+++ controllers/videoListController.js
@@ -1,6 +1,8 @@
 const videos = require("../utils/getVideos");
+const Video = require("../models/video");
 
 const videoListController = (req, res) => {
+	videos.forEach((video) => Video.create({ path: video, hasPlayed: false }));
 	res.send(videos);
 };
 
