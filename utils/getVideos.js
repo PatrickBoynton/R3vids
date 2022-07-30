4d22d97e8cc110ce78cc779ddcf102a4db16a851
PatrickBoynton <jpboynton3@gmail.com>
2022-07-30 18:20:35 -0400

    Filtered out the things that weren't videos.

--- utils/getVideos.js
+++ utils/getVideos.js
@@ -3,8 +3,8 @@ const displayPath = require("./displayPath");
 
 const tree = directoryTree("D:\\Extras\\");
 
-const videos = tree.children.map((child) => {
-	if (child.name.endsWith(".mp4")) return displayPath + child.name;
-});
+const videos = tree.children
+	.filter((child) => child.name.endsWith(".mp4"))
+	.map((child) => displayPath + child.name);
 
 module.exports = videos;
