2dd323518b96d6e5224877caf1e1df2399b172d4
PatrickBoynton <jpboynton3@gmail.com>
2022-08-14 14:16:50 -0400

    Added a check for localhost.

--- utils/database.js
+++ utils/database.js
@@ -2,7 +2,7 @@ const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize("playground", "root", "safepass1", {
 	dialect: "mysql",
-	host: "localhost",
+	host: "localhost" || "mysql",
 });
 
 module.exports = sequelize;
