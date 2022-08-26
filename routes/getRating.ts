4fde199f561f7ce17131537b04a8309caa7172d7
PatrickBoynton <jpboynton3@gmail.com>
2022-08-26 00:07:28 -0400

    Converted getRating to a ts module and exported it.

--- routes/getRating.ts
+++ routes/getRating.ts
@@ -1,7 +1,9 @@
-const { Router } = require("express");
+import { Router } from "express";
 
 const router = Router();
 
 router.get("/Video", (req, res) => {
 	res.send("GET RATINGS");
 });
+
+export default router;
