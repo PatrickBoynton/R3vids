d0e4e94cf150ebf2eb6a0a05a5cac4cf412ca8d6
PatrickBoynton <jpboynton3@gmail.com>
2022-08-28 01:02:44 -0400

    Added search functionality.

--- client/src/VideoList.tsx
+++ client/src/VideoList.tsx
@@ -5,17 +5,23 @@ import {
     Typography,
 } from "@mui/material";
 import useStore from "./utils/store";
+import { useState } from "react";
 import "./VideoList.css";
 
 
 const VideoList = ({title='', videos = [], playedVideos = [], search = false}) => {
     const {getSingleVideo} = useStore();
-
+    const [value, setValue] = useState("");
+    
+    const handleChange = (e: any) => {
+      setValue(e.target.value);
+    }
+    
     return (
         <>
            <Typography variant='h2'>{title}</Typography> 
-           {search ? <Input /> : null}
-           {(videos || playedVideos).map((video: string, index: number) =>
+           {search ? <Input onChange={handleChange} /> : null}
+           {(videos || playedVideos).filter((video: string) => video.toLowerCase().includes(value)).map((video: string, index: number) =>
           <>
             <ListItemButton key={index} component="a" href="#">        
               <ListItemText primary={video} onClick={(e: any) => getSingleVideo(e.target.innerHTML, index)}/> 
