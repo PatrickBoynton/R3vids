da8e1a17d19ff3e5a97bd7b1f4587c5f4fac4bfe
PatrickBoynton <jpboynton3@gmail.com>
2022-08-30 13:19:43 -0400

    Added types.

--- routes/sortByType.ts
+++ routes/sortByType.ts
@@ -1,10 +1,12 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
+import IVideo from "../models/IVideo";
 import Video from "../models/video";
 
 const router = Router();
 
-router.get("/sort/", async (req, res) => {
-	const videos = await Video.findAll({ order: [["type", "DESC"]] });
+router.get("/sort/", async (req: Request, res: Response) => {
+	const videos: IVideo = await Video.findAll({ order: [["type", "DESC"]] });
+	
 	res.send(videos);
 });
 
