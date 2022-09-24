486c1ca5315efde2a801a39ff167e8e9734f8f5a
PatrickBoynton <jpboynton3@gmail.com>
2022-09-24 10:04:16 -0400

    Added a ref to the media.

--- client/src/VideoPlayer.tsx
+++ client/src/VideoPlayer.tsx
@@ -1,12 +1,14 @@
 import { CardMedia } from "@mui/material";
+import { useRef } from "react";
 import InfoBar from "./InfoBar";
 import useStore from "./utils/store";
 const VideoPlayer = () => {
     const {singleVideo, randomVideo} = useStore();
+    const videoRef = useRef(null);
     return (
         <>
-          <CardMedia component='video' src={singleVideo?.path || randomVideo?.path}  controls autoPlay muted/>
-          <InfoBar title={singleVideo?.path || randomVideo?.path} />
+          <CardMedia ref={videoRef}  component='video' src={singleVideo?.path || randomVideo?.path} controls autoPlay muted/>
+          <InfoBar title={singleVideo?.path || randomVideo?.path} button/>
         </>
     );
 };
