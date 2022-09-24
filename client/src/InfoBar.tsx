3a059753d6737e27d8a4a462d582ca82ebe989ae
PatrickBoynton <jpboynton3@gmail.com>
2022-09-24 10:07:01 -0400

    Working on setting rating to the database.

--- client/src/InfoBar.tsx
+++ client/src/InfoBar.tsx
@@ -1,25 +1,47 @@
-import { Rating, Button, Typography } from "@mui/material"
+import { Rating, Button, Typography, Select, MenuItem } from "@mui/material"
 import useStore from "./utils/store";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 interface Props {
   title: string | undefined;
+  button: boolean;
 }
 
-const InfoBar = ({title}: Props) => {
-    const { getRandomVideo } = useStore();
+const InfoBar = ({title, button}: Props) => {
+    const { getRandomVideo, getRandomPlayedVideo, getTypes, types, setRating} = useStore();
     const [value, setValue] = useState<number | null>();
+    
+    useEffect(() => {
+      getTypes()
+    }, [getTypes])
 
     return <div style={{border: '2px solid #383879', textAlign: 'center', padding: '10px'}}>
-                  <Typography variant='h4' sx={{color: '#fff', textAlign: 'center'}}>{title?.split("/Video/")[1]}</Typography>
+        <Typography variant='h4' sx={{color: '#fff', textAlign: 'center'}}>{title?.split("/Video/")[1]}</Typography>
+        <Select
+    labelId="demo-simple-select-label"
+    id="demo-simple-select"
+    value={types.map((x: any) => x.id)}
+    label="Type"
+    // onChange={handleChange}
+  >
+    {types.map((x: any) => <MenuItem value={10}>{x.type}</MenuItem>)}
+  </Select>
         <Button onClick={getRandomVideo}>Random</Button>
-        
-        <Rating name="simple-controlled" value={value} onChange={(event, newValue) => {
+        {button ? <Button onClick={() => getRandomPlayedVideo()}>Random Played Video</Button> : null}
+
+        <Rating 
+        name="controlled-rating" 
+        value={value} 
+        onChange={(event, newValue) => {
+          if(newValue! > 0) {
+            setRating(newValue);
           setValue(newValue);
+          } else {
+            setRating(0);
+            setValue(0)
+          }
         }}
       />
-        <Button>Back</Button>
-        <Button>Next</Button>
     </div>;
 }
 
